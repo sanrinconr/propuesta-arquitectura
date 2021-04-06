7167d02fb8b024cd25ac525d96d598123f6048c4
Santiago Rincon <santiagorincon2001@gmail.com>
2021-04-05 23:20:45 -0500

    Propuesta finalizada

--- api/index.js
+++ api/index.js
@@ -2,5 +2,21 @@ const app = require('./src/app');
 
 let server;
   server = app.listen(process.env.PORT || 3001, () => {
-    console.log(`Listening in http://localhost:3001/graphql`)
+    console.log(`Si se quiere, entrar y probar con:
+    {
+      posts {
+        title
+        description
+        author {
+          name
+          age
+        }
+      }
+      post(id:0) {
+        title
+        description
+      }
+  }`)
+  console.log(`Link: http://localhost:3001/graphql`)
+
 });
